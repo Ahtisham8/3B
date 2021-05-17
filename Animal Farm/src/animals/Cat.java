bd8a94ff73b340b6f94e69b852add9b54a922b2a
Muhammad Ahtisham <m.ahtisham54321@gmail.com>
2021-05-17 10:39:57 -0500

    Added Constructor
    Added a constructor that accepts all instance variables as parameters

--- Animal Farm/src/animals/Cat.java
+++ Animal Farm/src/animals/Cat.java
@@ -6,4 +6,19 @@ public class Cat
 	private String breed;
 	private String color;
 	private int age;
+	
+	/**
+	 * @param name
+	 * @param breed
+	 * @param color
+	 * @param age
+	 */
+	public Cat(String name, String breed, String color, int age) {
+		super();
+		this.name = name;
+		this.breed = breed;
+		this.color = color;
+		this.age = age;
+	}
+	
 }
